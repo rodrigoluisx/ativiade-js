9d77ded0e35582f0823f456e06a993b983e8498f
rodrigoluisx <rodrilasek02@gmail.com>
2026-06-23 20:11:28 -0300

    atividade

---
+++ atividade_switch.js
@@ -0,0 +1,24 @@
+let codigo_pedido = 1
+let quantidade = 2
+
+switch (codigo_pedido){
+case 1:
+    console.log ("pizza")
+    console.log ("pedido tradicional")
+break
+case 2:
+    console.log ("hamburger")
+    console.log ("pedido tradicional")
+break
+case 3:
+    console.log ("salada")
+    console.log ("pedido saudavel")
+break
+case 4:
+    console.log ("refrigerante")
+break
+default:
+    console.log ("produto invalido")
+break
+console.log (codigo_pedido = 4) ? "bebida escolhida" : "alimento escolhido"
+}
